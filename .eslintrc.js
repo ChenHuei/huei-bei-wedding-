e22f13fd10d2c531072c2f312e378b3cb127f244
leo <leo.chen@cloud-interactive.com>
2021-03-11 17:09:37 +0800

    Modify setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,12 @@ module.exports = {
     browser: true,
     node: true,
   },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
+  },
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'prettier',
@@ -13,5 +19,28 @@ module.exports = {
   ],
   plugins: ['prettier'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    camelcase: 'off',
+    curly: 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'handle-callback-err': 'off',
+    'no-template-curly-in-string': 'off',
+    'space-before-function-paren': 'off',
+    'arrow-parens': 'off',
+    'prettier/prettier': 'error',
+    'nuxt/no-cjs-in-config': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+  },
 }
