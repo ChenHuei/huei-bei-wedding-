0b02cbbb534a36861c6ddf2b9f60997383d490bb
leo <leo.chen@cloud-interactive.com>
2021-03-12 18:04:31 +0800

    Modify index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <main>
     <Banner />
+    <Story :list="imageList" />
+    <Details />
   </main>
 </template>
 
@@ -8,5 +10,22 @@
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
-export default class Index extends Vue {}
+export default class Index extends Vue {
+  imageList: string[] = []
+
+  mounted(): void {
+    this.getImageList()
+  }
+
+  // methods
+
+  async getImageList(): Promise<void> {
+    const list = await this.$fire.storage
+      .ref()
+      .listAll()
+      .then((result) => result.items.map((item) => item.getDownloadURL()))
+
+    this.imageList = await Promise.all(list).then((url) => url)
+  }
+}
 </script>
