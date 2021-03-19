1c4edf918822e226289f4babc8882fed22e0b0ab
leo <leo.chen@cloud-interactive.com>
2021-03-19 17:27:12 +0800

    Add back top component

---
+++ components/BackTop.vue
@@ -0,0 +1,61 @@
+<template>
+  <transition name="fade">
+    <div
+      v-show="isDisplay"
+      class="back-top fixed w-12 h-12 flex justify-center items-center bg-primary rounded-full cursor-pointer duration-500"
+      @click="scrollToTop"
+    >
+      <ArrowDownSvg class="w-6 h-6" />
+    </div>
+  </transition>
+</template>
+
+<script lang="ts">
+/**
+ *  @author leo.chen 2021-03-19
+ *  @description description
+ *
+ *  @summary summary
+ */
+
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+// images
+import ArrowDownSvg from '@/assets/images/ArrowDown.svg?inline'
+
+@Component({
+  components: {
+    ArrowDownSvg,
+  },
+})
+export default class BackTop extends Vue {
+  @Prop()
+  current!: number
+
+  @Prop()
+  innerHeight!: number
+
+  get isDisplay(): boolean {
+    return this.current > this.innerHeight / 2
+  }
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.back-top {
+  right: 20px;
+  bottom: 20px;
+  transform: rotate(180deg);
+
+  &:hover {
+    opacity: 0.6;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
     <Details ref="details" />
     <Map ref="map" />
     <Event ref="event" />
+    <BackTop :current="currentYOffset" :inner-height="innerHeight" />
   </main>
 </template>
 
@@ -27,6 +28,9 @@ import { MenuItem } from '@/types'
   },
 })
 export default class Index extends Vue {
+  currentYOffset = 0
+  innerHeight = 0
+
   imageList: string[] = []
 
   menuList: MenuItem[] = [
@@ -41,15 +45,19 @@ export default class Index extends Vue {
       // init
       await this.getImageList()
       this.onResize()
+      this.onScroll()
     })
 
     // event listener
-    const debounceOnReisze = debounce(this.onResize, 500)
+    const debounceOnReisze = debounce(this.onResize, 300)
+    const debounceOnScroll = debounce(this.onScroll, 100)
 
     window.addEventListener('resize', debounceOnReisze)
+    window.addEventListener('scroll', debounceOnScroll)
 
     this.$once('hook:beforeDestroy', (): void => {
       window.removeEventListener('resize', debounceOnReisze)
+      window.removeEventListener('scroll', debounceOnScroll)
     })
   }
 
@@ -65,11 +73,19 @@ export default class Index extends Vue {
   }
 
   onResize(): void {
+    // set menu item offsetTop
     this.menuList.forEach((item, index) => {
       const target = (this.$refs[item.title] as Vue).$el as HTMLElement
 
       this.$set(this.menuList[index], 'offsetTop', target.offsetTop)
     })
+
+    // set inner height
+    this.innerHeight = window.innerHeight
+  }
+
+  onScroll(): void {
+    this.currentYOffset = window.pageYOffset
   }
 }
 </script>
