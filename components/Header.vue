25c45c34788df09b9ffa175201791a8f1a8ab6ea
leo <leo.chen@cloud-interactive.com>
2021-03-23 11:40:32 +0800

    Modify header component

--- components/Header.vue
+++ components/Header.vue
@@ -1,25 +1,12 @@
 <template>
-  <header class="header fixed w-full h-auto p-4">
-    <div
-      class="w-full max-w-screen-lg flex justify-between items-center mx-auto"
-    >
-      <div
-        class="w-auto text-primary uppercase cursor-pointer"
-        @click="scrollTo"
-      >
-        {{ title }}
-      </div>
-      <div class="w-auto flex -mr-4">
-        <div
-          v-for="item in list"
-          :key="item.title"
-          class="mx-4 text-primary uppercase cursor-pointer"
-          @click="scrollTo(item.offsetTop)"
-        >
-          {{ item.title }}
-        </div>
-      </div>
-    </div>
+  <header class="header fixed w-full h-auto p-4 flex justify-end">
+    <HeaderDesktop
+      class="hidden md:flex"
+      :title="title"
+      :list="list"
+      @scroll="scrollTo"
+    />
+    <HeaderHamburger class="md:hidden" />
   </header>
 </template>
 
@@ -46,7 +33,7 @@ export default class Header extends Vue {
 
   // methods
 
-  scrollTo(top: number = 0): void {
+  scrollTo(top: number): void {
     window.scrollTo({
       top,
       behavior: 'smooth',
---
+++ components/header/Desktop.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="w-full max-w-screen-lg justify-between items-center mx-auto">
+    <div class="w-auto text-primary uppercase cursor-pointer" @click="scroll">
+      {{ title }}
+    </div>
+    <div class="w-auto flex -mr-4">
+      <div
+        v-for="item in list"
+        :key="item.title"
+        class="mx-4 text-primary uppercase cursor-pointer"
+        @click="scroll(item.offsetTop)"
+      >
+        {{ item.title }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+/**
+ *  @author leo.chen 2021-03-23
+ *  @description description
+ *
+ *  @summary summary
+ */
+
+import { Component, Vue, Prop, Emit } from 'vue-property-decorator'
+
+// types
+import { MenuItem } from '@/types'
+
+@Component
+export default class HeaderDesktop extends Vue {
+  @Prop()
+  title!: string
+
+  @Prop()
+  list!: MenuItem[]
+
+  @Emit()
+  scroll(top: number = 0): number {
+    return top
+  }
+}
+</script>
