4bb0db6ba7ea6452aa792d6fe84e3def2ecef601
leo <leo.chen@cloud-interactive.com>
2021-03-25 18:08:13 +0800

    Modify layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,9 @@
 <template>
-  <Nuxt />
+  <div>
+    <Header :title="title" :list="menuList" />
+    <nuxt-child ref="content" @init="init" />
+    <BackTop :current="currentYOffset" :inner-height="innerHeight" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -11,7 +15,68 @@
  */
 
 import { Component, Vue } from 'vue-property-decorator'
+import { debounce } from 'vue-debounce'
 
-@Component
-export default class DefaultLayout extends Vue {}
+// constants
+import { PROJECT_TITLE, SectionNameNouns } from '@/constants'
+
+// types
+import { MenuItem } from '@/types'
+
+@Component({
+  data() {
+    return {
+      title: PROJECT_TITLE,
+    }
+  },
+})
+export default class DefaultLayout extends Vue {
+  currentYOffset = 0
+  innerHeight = 0
+
+  menuList: MenuItem[] = [
+    { title: SectionNameNouns.story, offsetTop: 0 },
+    { title: SectionNameNouns.details, offsetTop: 0 },
+    { title: SectionNameNouns.map, offsetTop: 0 },
+    { title: SectionNameNouns.event, offsetTop: 0 },
+  ]
+
+  mounted(): void {
+    const debounceOnReisze = debounce(this.onResize, 300)
+    const debounceOnScroll = debounce(this.onScroll, 100)
+
+    window.addEventListener('resize', debounceOnReisze)
+    window.addEventListener('scroll', debounceOnScroll)
+
+    this.$once('hook:beforeDestroy', (): void => {
+      window.removeEventListener('resize', debounceOnReisze)
+      window.removeEventListener('scroll', debounceOnScroll)
+    })
+  }
+
+  // methods
+
+  init(): void {
+    this.onResize()
+    this.onScroll()
+  }
+
+  onResize(): void {
+    // set menu item offsetTop
+    this.menuList.forEach((item, index) => {
+      const target = ((this.$refs.content as Vue).$refs[item.title] as Vue)
+        .$el as HTMLElement
+
+      this.$set(this.menuList[index], 'offsetTop', target.offsetTop)
+    })
+
+    // set inner height
+    this.innerHeight = window.innerHeight
+  }
+
+  onScroll(): void {
+    // set current pageYOffset
+    this.currentYOffset = window.pageYOffset
+  }
+}
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,46 @@
 <template>
   <main>
-    <Header :title="title" :list="menuList" />
     <Banner />
     <Story :ref="SectionNameNouns.story" :list="imageList" />
     <Details :ref="SectionNameNouns.details" />
     <Map :ref="SectionNameNouns.map" />
     <Event :ref="SectionNameNouns.event" />
-    <BackTop :current="currentYOffset" :inner-height="innerHeight" />
   </main>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import { debounce } from 'vue-debounce'
+import { Component, Vue, Emit } from 'vue-property-decorator'
 
 // constants
-import { PROJECT_TITLE, SectionNameNouns } from '@/constants'
-
-// types
-import { MenuItem } from '@/types'
+import { SectionNameNouns } from '@/constants'
 
 @Component({
   data() {
     return {
-      title: PROJECT_TITLE,
       SectionNameNouns,
     }
   },
 })
 export default class Index extends Vue {
-  currentYOffset = 0
-  innerHeight = 0
+  @Emit()
+  init(): void {}
 
   imageList: string[] = []
 
-  menuList: MenuItem[] = [
-    { title: SectionNameNouns.story, offsetTop: 0 },
-    { title: SectionNameNouns.details, offsetTop: 0 },
-    { title: SectionNameNouns.map, offsetTop: 0 },
-    { title: SectionNameNouns.event, offsetTop: 0 },
-  ]
-
-  mounted(): void {
-    this.$nextTick(async () => {
-      // init
-      await this.getImageList()
-      this.onResize()
-      this.onScroll()
-    })
-
-    // event listener
-    const debounceOnReisze = debounce(this.onResize, 300)
-    const debounceOnScroll = debounce(this.onScroll, 100)
-
-    window.addEventListener('resize', debounceOnReisze)
-    window.addEventListener('scroll', debounceOnScroll)
-
-    this.$once('hook:beforeDestroy', (): void => {
-      window.removeEventListener('resize', debounceOnReisze)
-      window.removeEventListener('scroll', debounceOnScroll)
-    })
+  async mounted(): Promise<void> {
+    await this.setImageList()
+    this.init()
   }
 
   // methods
 
-  async getImageList(): Promise<void> {
+  async setImageList(): Promise<void> {
     const list = await this.$fire.storage
       .ref()
       .listAll()
       .then((result) => result.items.map((item) => item.getDownloadURL()))
 
     this.imageList = await Promise.all(list).then((url) => url)
-  }
-
-  onResize(): void {
-    // set menu item offsetTop
-    this.menuList.forEach((item, index) => {
-      const target = (this.$refs[item.title] as Vue).$el as HTMLElement
-
-      this.$set(this.menuList[index], 'offsetTop', target.offsetTop)
-    })
-
-    // set inner height
-    this.innerHeight = window.innerHeight
-  }
-
-  onScroll(): void {
-    this.currentYOffset = window.pageYOffset
   }
 }
 </script>
