9283b8348ec39c1cf067054811acc22bfe6d3505
leo <leo.chen@cloud-interactive.com>
2021-03-30 15:19:27 +0800

    Add bubble animation

---
+++ constants/bubble.ts
@@ -0,0 +1,16 @@
+export const BUBBLE_NUMBERS = 12
+
+export enum BubbleSizeNouns {
+  max = 50,
+  min = 20,
+}
+
+export enum BubbleDurationNouns {
+  min = 8,
+  max = 12,
+}
+
+export enum BubbleScaleNouns {
+  min = 1,
+  max = 3,
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <main>
+    <Bubble v-for="(item, index) in bubbleSizeList" :key="index" :size="item" />
     <Banner />
     <Story :ref="SectionNameNouns.story" :list="imageList" />
     <Details :ref="SectionNameNouns.details" />
@@ -10,9 +11,19 @@
 
 <script lang="ts">
 import { Component, Vue, Emit } from 'vue-property-decorator'
+import { gsap } from 'gsap'
 
 // constants
 import { SectionNameNouns } from '@/constants'
+import {
+  BubbleSizeNouns,
+  BubbleDurationNouns,
+  BubbleScaleNouns,
+  BUBBLE_NUMBERS,
+} from '@/constants/bubble'
+
+// utils
+import { getRandomBetween } from '@/utils/math'
 
 @Component({
   data() {
@@ -23,13 +34,38 @@ import { SectionNameNouns } from '@/constants'
 })
 export default class Index extends Vue {
   @Emit()
-  init(): void {}
+  init(): void {
+    gsap.utils.toArray('.bubble').forEach((bubble, i) => {
+      gsap.to(bubble as gsap.TweenTarget, {
+        duration: `random(${BubbleDurationNouns.min}, ${BubbleDurationNouns.max})`,
+        delay: i * 0.5,
+        repeat: -1,
+        top: 0,
+        opacity: 0,
+        scale: `random(${BubbleScaleNouns.min}, ${BubbleScaleNouns.max})`,
+        onRepeat: () => {
+          this.$set(
+            this.bubbleSizeList,
+            i,
+            getRandomBetween(BubbleSizeNouns.min, BubbleSizeNouns.max)
+          )
+        },
+      })
+    })
+  }
 
   imageList: string[] = []
 
+  bubbleSizeList: number[] = []
+
+  // mounted
+
   async mounted(): Promise<void> {
-    await this.setImageList()
-    this.init()
+    await Promise.all([this.setBubbleSizeList(), this.setImageList()])
+
+    this.$nextTick(() => {
+      this.init()
+    })
   }
 
   // methods
@@ -41,6 +77,15 @@ export default class Index extends Vue {
       .then((result) => result.items.map((item) => item.getDownloadURL()))
 
     this.imageList = await Promise.all(list).then((url) => url)
+  }
+
+  setBubbleSizeList(): Promise<void> {
+    return new Promise((resolve) => {
+      this.bubbleSizeList = Array(BUBBLE_NUMBERS)
+        .fill(0)
+        .map(() => getRandomBetween(BubbleSizeNouns.min, BubbleSizeNouns.max))
+      resolve()
+    })
   }
 }
 </script>
