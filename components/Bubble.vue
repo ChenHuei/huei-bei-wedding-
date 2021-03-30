81279333e1faaf06f949c79ceacae434342ebdf5
leo <leo.chen@cloud-interactive.com>
2021-03-30 15:18:44 +0800

    Add bubble component

---
+++ components/Bubble.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="bubble fixed bg-primary rounded-full opacity-70 z-10"
+    :style="getStyles"
+  ></div>
+</template>
+
+<script lang="ts">
+/**
+ *  @author leo.chen 2021-03-30
+ *  @description description
+ *
+ *  @summary summary
+ */
+
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+// utils
+import { getRandomBetween } from '@/utils/math'
+
+@Component
+export default class Bubble extends Vue {
+  @Prop()
+  size!: number
+
+  get getStyles(): { [key: string]: string | number } {
+    return {
+      left: `${getRandomBetween(0, 100)}%`,
+      bottom: `-${this.size}px`,
+      width: `${this.size}px`,
+      height: `${this.size}px`,
+    }
+  }
+}
+</script>
