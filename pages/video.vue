647b4973c2e9f748da0d811786a12987326f2dac
leo <leo.chen@cloud-interactive.com>
2021-05-06 11:52:26 +0800

    Add message image in video page

--- pages/video.vue
+++ pages/video.vue
@@ -17,9 +17,15 @@ import { gsap } from 'gsap'
 import { SPEED_BASE, SIZES_BASE, COLOR_LIST } from '@/constants/video'
 
 // types
+interface User {
+  displayName: string
+  pictureUrl: string
+}
+
 interface Message {
   id: number
   message: string
+  user: User
 }
 
 @Component({
@@ -27,48 +33,74 @@ interface Message {
 })
 export default class App extends Vue {
   mounted(): void {
-    const eventSource = new EventSource(
-      'https://wedding-line-bot.herokuapp.com/messages'
-    )
+    const eventSource = new EventSource(`${process.env.API_URL}messages`)
 
     eventSource.onmessage = (e) => {
-      const messages: string[] = JSON.parse(e.data).messages
+      const messages: {
+        user: User
+        text: string
+      }[] = JSON.parse(e.data).messages
 
-      messages.forEach((message: string) => {
+      messages.forEach((message) => {
         this.createText({
           id: new Date().getTime(),
-          message,
+          message: message.text,
+          user: message.user,
         })
       })
     }
   }
 
   async createText(item: Message): Promise<void> {
-    const { clientWidth, clientHeight } = document.documentElement
-    const textEl = document.createElement('div')
+    const textEl = this.getTextElement(item.id, document.documentElement)
+    const userEl = this.getUserElement(item.user.pictureUrl)
 
-    textEl.id = `text${item.id}`
+    textEl.appendChild(userEl)
     textEl.appendChild(document.createTextNode(item.message))
 
     document.body.appendChild(textEl)
 
+    await gsap.to(`#${textEl.id}`, {
+      duration: this.getRandomSpeed(),
+      x: -1 * (document.documentElement.clientWidth + textEl.clientWidth),
+    })
+
+    textEl?.parentNode?.removeChild(textEl)
+  }
+
+  getUserElement(url: string): HTMLDivElement {
+    const userEl = document.createElement('div')
+
+    userEl.style.width = '32px'
+    userEl.style.height = '32px'
+    userEl.style.borderRadius = '50%'
+    userEl.style.backgroundImage = `url(${url})`
+    userEl.style.backgroundSize = 'cover'
+    userEl.style.marginRight = '8px'
+
+    return userEl
+  }
+
+  getTextElement(id: number, documentElement: HTMLElement): HTMLDivElement {
+    const textEl = document.createElement('div')
+
+    textEl.id = `text${id}`
+
     textEl.style.position = 'fixed'
     textEl.style.top = `${
-      Math.random() * (clientHeight - textEl.clientHeight)
+      Math.random() * (documentElement.clientHeight - textEl.clientHeight)
     }px`
-    textEl.style.left = `${clientWidth}px`
+    textEl.style.left = `${documentElement.clientWidth}px`
+
+    textEl.style.display = 'flex'
+    textEl.style.alignItems = 'center'
 
     textEl.style.fontSize = `${this.getRandomSize()}px`
     textEl.style.fontWeight = '900'
     textEl.style.whiteSpace = 'nowrap'
     textEl.style.color = this.getRandomColor()
 
-    await gsap.to(`#${textEl.id}`, {
-      duration: this.getRandomSpeed(),
-      x: -1 * (clientWidth + textEl.clientWidth),
-    })
-
-    textEl?.parentNode?.removeChild(textEl)
+    return textEl
   }
 
   getRandomColor() {
