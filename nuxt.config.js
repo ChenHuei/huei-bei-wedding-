ced74d2fff2cd1209bc2d5ae8672de842de17d96
leo <leo.chen@cloud-interactive.com>
2021-05-10 10:01:54 +0800

    Modify config

--- nuxt.config.js
+++ nuxt.config.js
@@ -246,6 +246,9 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     vendor: ['scrollmagic'],
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
+    },
   },
 
   stylelint: {
